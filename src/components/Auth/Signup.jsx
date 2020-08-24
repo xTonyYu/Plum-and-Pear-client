a604e3c6b28616ab220212ae91b293eefdf50ca0
xTonyYu <xtonyyu@gmail.com>
2020-08-24 16:02:57 -0700

    signup page done

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 class Signup extends React.Component {
   state = {
@@ -8,6 +9,22 @@ class Signup extends React.Component {
     password: '',
     firstName: '',
     lastName: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post(`${process.env.REACT_APP_API}/auth/register`, this.state)
+    .then(res => {
+      console.log(res);
+      this.props.history.push('/login')
+    })
+    .catch(err => console.log('err login...', err))
   }
 
   render() {
