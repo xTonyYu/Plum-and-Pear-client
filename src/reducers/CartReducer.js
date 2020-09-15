7aa0679cb84cc49bcd6c45af0335d3bd28a770a0
xTonyYu <xtonyyu@gmail.com>
2020-09-15 10:08:11 -0700

    initial redux conversion setup

---
+++ src/reducers/CartReducer.js
@@ -0,0 +1,4 @@
+const defaultState = {
+  numItems: 0,
+  
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import cart from './CartReducer'
+
+const rootReducer = combineReducers({
+  cart
+})
